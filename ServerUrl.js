40cf60976abd8db79ac61c304956db72158c1289
Afcrea <mr1998k@gmail.com>
2024-12-04 00:12:19 +0900

    Update ServerUrl.js

--- ServerUrl.js
+++ ServerUrl.js
@@ -1,2 +1,2 @@
-const apiServer = `http://211.188.55.250/api`;
-const Server = `http://211.188.55.250`;
+const apiServer = `http://211.188.55.250:8070/api`;
+const Server = `http://211.188.55.250:8080`;
