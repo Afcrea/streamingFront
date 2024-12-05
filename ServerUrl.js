d2b721de8bd079187f89f136110b27c6c99e5d63
Afcrea <mr1998k@gmail.com>
2024-12-06 00:43:41 +0900

    Update ServerUrl.js

--- ServerUrl.js
+++ ServerUrl.js
@@ -1,3 +1,3 @@
 var apiServer = `http://211.188.55.250:8070/api`;
 var Server = `http://211.188.55.250:8080`;
-var StreamServer = `http://211.188.55.250:8060`;
+var StreamServer = `http://211.188.55.250:8050`;
